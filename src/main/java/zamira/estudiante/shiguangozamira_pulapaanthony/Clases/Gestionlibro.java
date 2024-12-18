ecc6424da1e8eee4d79e87819551fb11773bae67
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2024-12-18 07:32:31 -0500

    arreglado

--- src/main/java/zamira/estudiante/shiguangozamira_pulapaanthony/Clases/Gestionlibro.java
+++ src/main/java/zamira/estudiante/shiguangozamira_pulapaanthony/Clases/Gestionlibro.java
@@ -13,36 +13,47 @@ public class Gestionlibro {
    private String titulo;
    private String autor;
    private double precio;
-   Private TipoGenero tipoGenero;
+   private TipoGenero tipoGenero;
 
-    public Gestionlibro(String titulo, String autor, double precio, TipoGenero tipoGenero ) {
+    public Gestionlibro(String titulo, String autor, double precio, TipoGenero tipoGenero) {
         this.titulo = titulo;
         this.autor = autor;
         this.precio = precio;
         this.tipoGenero = tipoGenero;
-        
     }
 
     public String getTitulo() {
         return titulo;
     }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
     public String getAutor() {
         return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
     }
 
     public double getPrecio() {
         return precio;
     }
 
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
     public TipoGenero getTipoGenero() {
         return tipoGenero;
     }
-   
-   
 
-   
-   
-   
+    public void setTipoGenero(TipoGenero tipoGenero) {
+        this.tipoGenero = tipoGenero;
+    }
+    
     
 }
+   
--- src/main/java/zamira/estudiante/shiguangozamira_pulapaanthony/ShiguangoZamira_PulapaAnthony.java
+++ src/main/java/zamira/estudiante/shiguangozamira_pulapaanthony/ShiguangoZamira_PulapaAnthony.java
@@ -5,6 +5,8 @@
 package zamira.estudiante.shiguangozamira_pulapaanthony;
 
 import zamira.estudiante.shiguangozamira_pulapaanthony.Clases.Cliente;
+import zamira.estudiante.shiguangozamira_pulapaanthony.Clases.Compras;
+import zamira.estudiante.shiguangozamira_pulapaanthony.Clases.Gestionlibro;
 import zamira.estudiante.shiguangozamira_pulapaanthony.Clases.TipoGenero;
 
 /**
@@ -20,8 +22,21 @@ public class ShiguangoZamira_PulapaAnthony {
         Cliente cliente = new Cliente("Anita Suares", 123456789-7);
         Cliente cliente2 = new Cliente("Juli Grefa", 1342568950-7);
         
+       
         
-        Gestionlibro Dulce= new Gestionlibro("pirata del carive","Joinider", TipoGenero.FICCION);
+        
+        Gestionlibro Peliculo = new Gestionlibro("pirata del carive","Joinider",8.75, TipoGenero.FICCION);
+        Gestionlibro pelicula2 = new Gestionlibro("pirata del carive","Joinider",22.75, TipoGenero.NOFICCION);
+        
+        Compras compra = new Compras();
+        
+        compra.mostrarDetallesLibro(Peliculo);
+        compra.mostrarDetallesLibro(pelicula2);
+        
+        compra.mostarClientesLibros(cliente, Peliculo);
+        compra.mostarClientesLibros(cliente2, pelicula2);
+        
+      
         
         
     
